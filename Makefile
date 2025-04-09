ae9a867ebfc05887e151971d211081296713313a
Jill Stingray <jerryhouau@gmail.com>
2025-04-09 17:40:56 +0800

    added cunit test example

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 add.o: 
 	gcc ./src/add.c -o ./target/add.o -c
 
+test: add.o
+	gcc ./src/cunit_tests.c -o ./target/cunit_tests.o -c
+	gcc ./target/cunit_tests.o ./target/add.o -o ./target/cunit_target.o  -lcunit
+	./target/cunit_target.o
+
 check: add.o
 	gcc ./src/test.c -o ./target/test.o -c
 	gcc ./target/test.o ./target/add.o -o ./target/target.o 
---
+++ src/cunit_tests.c
@@ -0,0 +1,33 @@
+#include <CUnit/Basic.h>
+
+#include "include/add.h"
+
+void test_add(void) {
+    CU_ASSERT(add(7, 10) == 17);
+}
+
+void test_add_fail(void) {
+    CU_ASSERT(add(7, 10) == 10);
+}
+
+int main() {
+    CU_pSuite suite = NULL;
+    if (CUE_SUCCESS != CU_initialize_registry()) return CU_get_error();
+    suite = CU_add_suite("add_suite", NULL, NULL);
+    if (suite == NULL) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(suite, "test of add()", test_add) ||
+        NULL == CU_add_test(suite, "Intentionally failing test of add()",
+                            test_add_fail)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+    return CU_get_error();
+}
