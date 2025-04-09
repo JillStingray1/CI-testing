e0dc86a73666dbe58ec52ec983af4ee83fe96243
Jill Stingray <jerryhouau@gmail.com>
2025-04-09 18:56:32 +0800

    tests exit with error code

--- src/cunit_tests.c
+++ src/cunit_tests.c
@@ -29,5 +29,8 @@ int main() {
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-    return CU_get_error();
+    if (CU_get_error() != CUE_SUCCESS) {
+        return CU_get_error();
+    }
+    return CU_get_number_of_failures() == 0;
 }
